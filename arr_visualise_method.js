7ea8caff5a994014446269542750c08309a8c093
Harish <harish210430@gmail.com>
2024-11-19 03:05:41 -0800

    Add arr_visualise_method.js

---
+++ arr_visualise_method.js
@@ -0,0 +1,20 @@
+/*
+Array.forEach():
+*/
+// forEach() takes each element of the Array and calls the cbfn with that value(elements).
+// Return/Log: if u use console, it will show value in console.
+//  If u return the value, it will not do Anything to the value & it will be Garbag Collected.
+
+
+/*
+Array.map():
+*/
+// map(): each elements of Array passed to cbfn and Return a "same size of New Array".
+// If it did'nt return Anything then it will "return [undefined, undefined, ....., undefined]".
+
+
+/*
+Array.filter():
+*/
+// filter(): It only understand boolean value, It will always return "truthy" value And store it to an Array But what If u return undefined "A Falsy value" then it will always give us An Empty Array.
+
