6a2bed8a7f4404bfcdacb67d11aa14143f7f5ebc
Harish <harish210430@gmail.com>
2024-11-24 09:12:43 -0800

    filter values

--- assign_lvl11.js
+++ assign_lvl11.js
@@ -11,15 +11,89 @@ let words = [
   
   // - Write a function findLongestWord that takes an array of words and returns the longest word from the array. (Use above array "words" to test it). If there are 2 with the same length, it should return the first occurrence.
 function findLongestWord(word) {
-    words.sort((a, b) => b.length-a.length);
+    [...words].sort((a, b) => b.length-a.length);
     return words[0];
 };
 console.log(words);
 words.find(findLongestWord);
 
 // - Convert the above array "words" into an array of length of word instead of word.
+console.log(words);
 for(let i = 0; i < words.length; i++) {
-    console.log(words[i].length());
-    // words.push();
+   [...words][i] = words[i].length;
 }
-console.log(words[1].length);
+
+// Or
+words.map((word) => word.length);
+// console.log(words);
+
+// - Create a new array that only contains word with atleast one vowel.
+let wordAtleastOneVowel = [];
+for(let word of words) {
+  if(
+    word.includes('a') ||
+    word.includes('e') ||
+    word.includes('i') ||
+    word.includes('o') ||
+    word.includes('u')
+  ) {
+    wordAtleastOneVowel.push(word);
+  }
+}
+console.log(wordAtleastOneVowel);
+
+// Or
+let vowelsWord = words.filter((word) =>
+    {
+        if(
+        word.includes('a') ||
+        word.includes('e') ||
+        word.includes('i') ||
+        word.includes('o') ||
+        word.includes('u') 
+    ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+);
+console.log(vowelsWord);
+console.log(words);
+
+// - Find the index of the word "rhythm".
+console.log(words.indexOf('rhythm'));
+
+// - Create a new array that contians words not starting with vowel.
+let notStrtWithVowel = words.filter((word) => {
+    if(
+        word[0] === 'a' ||
+        word[0] === 'e' ||
+        word[0] === 'i' ||
+        word[0] === 'o' ||
+        word[0] === 'u' 
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+});
+console.log(notStrtWithVowel);
+
+
+// - Create a new array that contianse words not ending with vowel.
+
+let notEndsWithVowel = words.filter((word) => {
+    if(
+        word[word.length-1] === 'a' &&
+        word[word.length-1] === 'e' &&
+        word[word.length-1] === 'i' &&
+        word[word.length-1] === 'o' &&
+        word[word.length-1] === 'u' 
+    ) {
+        return false;
+    } else {
+        return true;
+    }
+});
+console.log(notEndsWithVowel);
