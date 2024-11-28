6d24646db1bd74b9f1b77b9712cb9777be98cc02
Harish <harish210430@gmail.com>
2024-11-28 06:15:12 -0800

    update assign_lvl11.js

--- assign_lvl11.js
+++ assign_lvl11.js
@@ -67,6 +67,12 @@ console.log(words.indexOf('rhythm'));
 // - Create a new array that contians words not starting with vowel.
 let notStrtWithVowel = words.filter((word) => {
     if(
+        // !word.startsWith('a') &&
+        // !word.startsWith('e') &&
+        // !word.startsWith('i') &&
+        // !word.startsWith('o') &&
+        // !word.startsWith('u')  
+
         word[0] === 'a' ||
         word[0] === 'e' ||
         word[0] === 'i' ||
@@ -85,15 +91,116 @@ console.log(notStrtWithVowel);
 
 let notEndsWithVowel = words.filter((word) => {
     if(
-        word[word.length-1] === 'a' &&
-        word[word.length-1] === 'e' &&
-        word[word.length-1] === 'i' &&
-        word[word.length-1] === 'o' &&
-        word[word.length-1] === 'u' 
+        // word[word.length-1] === 'a' ||
+        // word[word.length-1] === 'e' ||
+        // word[word.length-1] === 'i' ||
+        // word[word.length-1] === 'o' ||
+        // word[word.length-1] === 'u' 
+
+        !word.endsWith('a') &&
+        !word.endsWith('e') &&
+        !word.endsWith('i') &&
+        !word.endsWith('o') &&
+        !word.endsWith('u') 
     ) {
-        return false;
-    } else {
         return true;
+    } else {
+        return false;
     }
 });
 console.log(notEndsWithVowel);
+
+
+let numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+
+// - Create a sumArray function that takes an array of number as a parameter, and calculate the sum of all its numbers.
+let sumArray = (arr) => {
+    return arr.reduce((acc, num) => {
+        return acc + num;
+    }, 0);
+};
+console.log(sumArray(numbers));
+
+// Or
+// function sumArray(arr) {
+//     arr.forEach(num => {
+//         sum = sum + num;
+//     });
+//     return sum;
+// };
+// console.log(sumArray(numbers));
+
+// function sumArray(arr) {
+//     return arr.reduce((acc, num) => {
+//         return acc + num;
+//     }, 0);
+// };
+// console.log(sumArray(numbers));
+
+
+// - Make a new array that contains number multiplied by 3 like [6, 18, 27 ...].
+let multipliedBy3 = numbers.filter(num => num % 3 === 0);
+console.log(multipliedBy3);
+
+// - Create a new array that contains only even numbers.
+let evenNumbers = numbers.filter(num => num%2 === 0);
+console.log(evenNumbers);
+
+// - Create  a new array that contains only odd numbers.
+let oddNumbers = numbers.filter(num => num%2 != 0);
+console.log(oddNumbers);
+
+// - Create a new array that should have true for even number and false for odd numbers.
+let returnTrue = numbers.map(num => {
+    if(num % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+});
+console.log(returnTrue);
+
+// - Sort the above number in assending order.
+[...numbers].sort((a, b) => a-b);
+
+// - Does sort mutate the original array?
+/*
+Yes, it mutates the original Array.
+*/
+
+// - Find the sum of the numbers in the array.
+let sumOfNums = ((acc, num) => acc + num);
+console.log(numbers.reduce(sumOfNums));
+
+//- Write a function averageNumbers that receives an array of numbers and calculate the average of the numbers.
+let averageNumbers = (arr) => {
+    let sum = arr.reduce((acc, n) => {
+        return acc + n;
+    }, 0);
+    return sum / arr.length;
+};
+console.log(averageNumbers(numbers));
+
+
+let strings = [
+    'seat',
+    'correspond',
+    'linen',
+    'motif',
+    'hole',
+    'smell',
+    'smart',
+    'chaos',
+    'fuel',
+    'palace',
+  ];
+  
+  // - Write a function averageWordLength that receives an array of words2 and calculate the average length of the words.
+  let averageWordLength = (arr) => {
+    let sumOfWords = arr.reduce((acc, word) => {
+        return acc + word.length;
+    }, 0);
+    return sumOfWords / arr.length;
+  };
+  console.log(averageWordLength(strings));
+
